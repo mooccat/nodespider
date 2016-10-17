1526e0c8f396a8b07ff4eb15d7e29e40922024ef
JHLiu <825681860@qq.com>
2016-10-17 08:34:34 +0800

    爬取冷笑话

--- index.js
+++ index.js
@@ -10,32 +10,31 @@ var count = 0;
 superagent
 	.get(sourceUrl)
 	.end(function(err,res){
-			if(res.status==200){
-				var $ = cheerio.load(res.text);
-				var maxPage = $(".joke_wrap").children().eq(1)[0].attribs.fx.split("pagination")[1].split(";")[1].split("=")[1];
-				for(var i= 0;i<maxPage;i++){
-					var query = {"page_num":i+1};
+	var $ = cheerio.load(res.text);
+	var maxPage = $(".joke_wrap").children().eq(1)[0].attribs.fx.split("pagination")[1].split(";")[1].split("=")[1];
+	for(var i= 0;i<maxPage;i++){
+		var query = {"page_num":i+1};
+		superagent
+			.get(sourceUrl)
+			.query(query)
+			.end(function(err,res){
+				if(res != undefined && res.status==200){
+					var $ = cheerio.load(res.text);
+					var joker_num = $(".joke_li")[0].attribs.id.replace(/[^0-9]/ig,"");
+					var url = baseUrl + joker_num;
 					superagent
-						.get(sourceUrl)
-						.query(query)
+						.get(url)
 						.end(function(err,res){
-							if(res != undefined){
-								var $ = cheerio.load(res.text);
-								var joker_num = $(".joke_li")[0].attribs.id.replace(/[^0-9]/ig,"");
-								var url = baseUrl + joker_num;
-								superagent
-									.get(url)
-									.end(function(err,res){
-										var $ = cheerio.load(res.text,{decodeEntities: false});
-										// console.log($("#joke_content_226343").children().html());
-										count++;
-										console.log(count);
-										urls.set(joker_num,true);
-									})
-							}	
+							if(res != undefined && res.status==200){
+								var $ = cheerio.load(res.text,{decodeEntities: false});
+								// console.log($("#joke_content_226343").children().html());
+								count++;
+								console.log(count);
+								urls.set(joker_num,true);
+							}
 						})
-			}
+				}	
+			})
 		}
-
 	});
 
